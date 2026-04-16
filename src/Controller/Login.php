e89a8d5217c5063f08a1a60ddc41c4ee285134ed
Lucile <lucileglad@gmail.com>
2026-04-16 10:37:11 +0200

    16.04, Fusion page login

--- src/Controller/Login.php
+++ src/Controller/Login.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class Login extends AbstractController
 {
@@ -19,10 +20,8 @@ class Login extends AbstractController
 
     }
 
-    #[Route('/rest/auth/login', methods: ['POST'])]
-    public function processLogin(Request $request, UserRepository $userRepository): Response 
-    {
-        dd("Salut");
+    #[Route('/login', methods: ['POST'])]
+    public function postLogin(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $hasher): Response    {
         $login = $request->request->get('login');
         $password = $request->request->get('password');
 
@@ -30,7 +29,26 @@ class Login extends AbstractController
 
     if ($userLogin == null) {
         return $this->render('defaite.html.twig');
-    } elseif ($userLogin->getPassword() === $password) {
+    } elseif ($hasher->isPasswordValid($userLogin, $password)) {        
+    return $this->render('succes.html.twig', [
+            'login' => $login,
+        ]);
+    } else {
+        return $this->render('defaite.html.twig');
+    }
+}
+
+    #[Route('/rest/auth/login', methods: ['POST'])]
+    public function processLogin(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $hasher) : Response 
+    {
+        $login = $request->request->get('login');
+        $password = $request->request->get('password');
+
+        $userLogin = $userRepository->findOneBy(['login' => $login]);
+
+    if ($userLogin == null) {
+        return $this->render('defaite.html.twig');
+    } elseif ($hasher->isPasswordValid($userLogin, $password)) {        
         return $this->render('succes.html.twig', [
             'login' => $login,
         ]);
